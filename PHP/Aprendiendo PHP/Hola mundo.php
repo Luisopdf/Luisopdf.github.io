8b0a1e68196839ebb170dab78c44a0fc01c09f8f
Luisopdf <168942655+Luisopdf@users.noreply.github.com>
2025-02-03 13:20:33 -0600

    Add files via upload

---
+++ PHP/Aprendiendo PHP/Hola mundo.php
@@ -0,0 +1,16 @@
+<?php
+
+    echo "Hola php\n";
+    $my_string = "Luis Cruz\n";
+    echo $my_string. " hola\n";
+
+    $numero1 = 1.3;
+    $numero2=2;
+    $resultado=$numero1+$numero2;
+    echo "El resultado es " . $resultado;
+    $my_string = 15;
+    echo gettype($my_string);
+    $datos = array("nombre" => "Juan", "edad" => 25, "ciudad" => "Madrid");
+    print_r($datos);
+
+?>
